df4898cacf60d9b702bec42052ed8081b2c2e2d6
Sven Otto <sven.otto@uni-bonn.de>
2020-06-25 17:33:57 +0200

    critical value fix

--- R/cusumprocess.R
+++ R/cusumprocess.R
@@ -61,8 +61,6 @@ get.cusumprocess <- function(formula, T){
 #' get.crit.Q.mon(1,Inf)
 #' get.crit.Q.mon(3,0.5)
 get.crit.Q.mon <- function(k, m = Inf, alternative = "two.sided"){
-  # load(file = "sysdata.rda")
-  Q.crit
   alphas <- as.numeric(colnames(Q.crit[[1]]))
   if(alternative == "one.sided") ( alphas <- alphas/2 )
   horizons <- as.numeric(rownames(Q.crit[[1]]))
@@ -131,7 +129,6 @@ get.crit.BQ <- function(k, alternative = "two.sided"){
 #' get.crit.SBQ.mon(1,Inf)
 #' get.crit.SBQ.mon(3,0.5)
 get.crit.SBQ.mon <- function(k, m = Inf, alternative = "two.sided"){
-  # SBQ.crit
   alphas <- as.numeric(colnames(SBQ.crit[[1]]))
   if(alternative == "one.sided") ( alphas <- alphas/2 )
   horizons <- as.numeric(rownames(SBQ.crit[[1]]))
