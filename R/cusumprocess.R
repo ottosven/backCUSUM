fd771146a3c11d3c7673648c91f319ba212da780
Sven Otto <sven.otto@uni-bonn.de>
2020-06-25 14:45:57 +0200

    critical values simulation added

--- R/cusumprocess.R
+++ R/cusumprocess.R
@@ -161,3 +161,108 @@ get.crit.SBQ.mon <- function(k, m = Inf, alternative = "two.sided"){
 get.crit.SBQ <- function(k, alternative = "two.sided"){
   return(get.crit.SBQ.mon(k, m=2, alternative)[[1]])
 }
+
+
+
+
+
+#' Simulates critical values for the multivariate forward CUSUM test
+#'
+#' The asymptotic critical values for the linear boundary \eqn{d(r) = 1+2r} are simulated based on \eqn{MC} Monte Carlo replications, where the limiting process under the null hypothesis is approximated on a grid of \eqn{T} equidistant points.
+#' The critical values of the retrospective backward CUSUM coincide with those of the forward CUSUM for m=2.
+#'
+#' @param k Number of regressors and dimension of the CUSUM process.
+#' @param m The length of the relative monitoring period; m = Inf for infinite horizon monitoring; for retrospective testing set m = 2 (default).
+#' @param MC Number of Monte Carlo replications.
+#' @param T gridsize.
+#' @param levels A vector if significance levels.
+#' @param alternative A character string specifying the alternative hypothesis; must be one of "two.sided" (default) or "one.sided".
+#'
+#' @return A vector of critical values for different significance levels.
+#' @export
+#'
+#' @examples
+#' sim.crit.Q(1, m=Inf)
+sim.crit.Q <- function(k, m = 2, levels = c(0.1, 0.05, 0.01, 0.001), alternative = "two.sided", MC = 1000, T = 10000){
+  r <- (1:T)/T
+  boundary.transf <- 1+r
+  if(m == Inf){
+    MRange <- 1:T
+  } else {
+    MRange <- 1:floor(T*(m-1)/m)
+  }
+  boundary.mrange <- boundary.transf[MRange]
+  BrownianMotion <- function(T)  ( cumsum(rnorm(T,0,sqrt(1/T))) )
+  sim.dist.Q <- function(...){
+    W <- sapply(rep(T,k), BrownianMotion)
+    B <- W - t(W[T,]*matrix(rep(r,k), nrow = k, byrow = TRUE))
+    B.mrange <- matrix(B[MRange,], ncol = k)
+    Q <- max((apply(abs(B.mrange), 1, max)/boundary.mrange))
+    return(Q)
+  }
+  CORE <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
+  realizations <- parallel::mcmapply(sim.dist.Q,1:MC,mc.cores = getOption("mc.cores", CORE))
+  if(alternative == "one.sided"){
+    quantiles <- quantile(realizations, 1-levels*2)
+  } else {
+    quantiles <- quantile(realizations, 1-levels)
+  }
+  names(quantiles) <- levels
+  return(quantiles)
+}
+
+
+
+
+#' Simulates critical values for the multivariate stacked backward CUSUM test
+#'
+#' The asymptotic critical values for the linear boundary \eqn{d(r) = 1+2r} are simulated based on \eqn{MC} Monte Carlo replications, where the limiting process under the null hypothesis is approximated on a grid of \eqn{T} equidistant points.
+#'
+#' @param k Number of regressors and dimension of the CUSUM process.
+#' @param m The length of the relative monitoring period; m = Inf for infinite horizon monitoring; for retrospective testing set m = 2 (default).
+#' @param MC Number of Monte Carlo replications.
+#' @param T gridsize.
+#' @param levels A vector if significance levels.
+#' @param alternative A character string specifying the alternative hypothesis; must be one of "two.sided" (default) or "one.sided".
+#'
+#' @return A vector of critical values for different significance levels.
+#' @export
+#'
+#' @examples
+#' sim.crit.SBQ(1, m=Inf)
+sim.crit.SBQ <- function(k, m = 2, levels = c(0.1, 0.05, 0.01, 0.001), alternative = "two.sided", MC = 100, T = 1000){
+  r <- (1:T)/T
+  # boundary <- 1+2*(m-1)*r
+  # boundary.transf <- 1+r
+  if(m == Inf){
+    MRange <- 2:T
+  } else {
+    MRange <- 2:floor(T*(m-1)/m)
+  }
+  # boundary.mrange <- boundary.transf[MRange]
+  BrownianMotion <- function(T)  ( cumsum(rnorm(T,0,sqrt(1/T))) )
+  getSBQMax <- function(rT, B){
+    sT <- 1:(rT-1)
+    bound <- (1-rT/T)*(1-sT/T)+2*(rT-sT)/T
+    Diff <- (1-sT/T)*matrix(rep(B[rT,],rT-1), nrow = rT-1, byrow = TRUE) - (1-rT/T)*B[sT,]
+    Inner <- matrix(abs(Diff), nrow = rT-1)
+    InnerMax <- apply(Inner, 1, max)
+    SBQ <- max(InnerMax/bound)
+  }
+  sim.dist.SBQ <- function(...){
+    W <- sapply(rep(T,k), BrownianMotion)
+    B <- W - t(W[T,]*matrix(rep(r,k), nrow = k, byrow = TRUE))
+    B.mrange <- matrix(B[c(1,MRange),], ncol = k)
+    SBQ <- max(sapply(MRange, getSBQMax, B=B.mrange))
+    return(SBQ)
+  }
+  CORE <- parallel::detectCores(all.tests = FALSE, logical = TRUE)
+  realizations <- parallel::mcmapply(sim.dist.SBQ,1:MC,mc.cores = getOption("mc.cores", CORE))
+  if(alternative == "one.sided"){
+    quantiles <- quantile(realizations, 1-levels*2)
+  } else {
+    quantiles <- quantile(realizations, 1-levels)
+  }
+  names(quantiles) <- levels
+  return(quantiles)
+}
